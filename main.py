3cf63e3996b9ddbf5ef8d57dfc1abb0d6d6f0a7f
bscheucher <122296588+bscheucher@users.noreply.github.com>
2025-03-27 23:50:42 +0100

    add coversation buffer memory

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
     MessagesPlaceholder
 )
+from langchain.memory import ConversationBufferMemory
 from langchain.agents import create_openai_functions_agent, AgentExecutor
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
 
 # Load API key
 load_dotenv()
@@ -16,17 +19,28 @@ api_key = os.getenv("OPENAI_API_KEY")
 # LLM instance
 chat = ChatOpenAI()
 
+tables = list_tables()
+
 # Corrected prompt template
 prompt = ChatPromptTemplate(
-    input_variables=["input", "agent_scratchpad"],  # Define expected input variables
     messages=[
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite database.\n"
+            f"The database has tables of: {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead, use the 'describe_tables' function"
+        ),
+        ),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 # Define tools
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 # Create agent
 agent = create_openai_functions_agent(
@@ -39,10 +53,14 @@ agent = create_openai_functions_agent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
 # Correct invocation
-response = agent_executor.invoke({"input": "How many users are in the database?"})
+response1 = agent_executor.invoke(
+    {"input": "How many orders are there? Write the results to an html report."})
+response2 = agent_executor.invoke(
+    {"input": "Execute the same process for users."})
 
-print(response)  # Print response for debugging
+print(response1, "\n", response2)  # Print response for debugging
